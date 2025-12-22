189c8ede3ec71276bb1d331b11b5d5474714a5eb
Clinttttt <clintvillanueva82@gmail.com>
2025-12-22 20:50:22 +0800

    initial changes

--- BlogApi.Infrastructure/Services/TokenService.cs
+++ BlogApi.Infrastructure/Services/TokenService.cs
@@ -22,7 +22,7 @@ namespace BlogApi.Infrastructure.Services
         {
             var claims = new List<Claim>
             {
-                new Claim(ClaimTypes.Name, user.UserName),
+                new Claim(ClaimTypes.Name, user.UserName!),
                 new Claim(ClaimTypes.NameIdentifier,user.Id.ToString())
             };
             var key = new SymmetricSecurityKey(
